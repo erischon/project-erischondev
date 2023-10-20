9adc32c58f7bd873847618258730bd3021dd78ae
erischon <66017574+erischon@users.noreply.github.com>
2023-10-20 15:40:30 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,32 +4,19 @@ import Link from "next/link";
 
 import { routes } from "@/data/routes";
 
-const getHash = () =>
-  typeof window !== "undefined"
-    ? decodeURIComponent(window.location.hash.replace("#", ""))
-    : undefined;
-
 /**
  * Renders a Navbar component.
  * @returns JSX.Element
  */
 export default function Navbar(): JSX.Element {
-  const pathname = `/#${getHash()}`;
-
-  const activeStyle = "text-amber-400 border-b-2 border-amber-400";
-
-  console.log(pathname);
-
   return (
     <nav>
       <ul className="flex gap-x-3">
-        {routes.map((route: any) => (
-          <li>
+        {routes.map((route: any, k) => (
+          <li key={k}>
             <Link
               href={route.url}
-              className={`text-lg font-semiBold hover:text-slate-300 ${
-                pathname === route.url ? activeStyle : ""
-              }`}
+              className={`text-lg font-semiBold hover:text-amber-400 hover:border-b-2 hover:border-amber-400`}
             >
               {route.name}
             </Link>
