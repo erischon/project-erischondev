3b50a2e0c335b5f3d914758584a536838ad3914e
erischon <66017574+erischon@users.noreply.github.com>
2023-10-21 12:23:11 +0200

    Update About.section.tsx

--- src/components/sections/About.section.tsx
+++ src/components/sections/About.section.tsx
@@ -10,40 +10,44 @@ export default function AboutSection() {
 
       <div className="grid grid-cols-12 gap-x-2 gap-y-5 mb-5 px-10 py-10 bg-slate-800">
         <div className="col-span-3">
-          <span className="text-8xl font-bold flex items-end">3+</span>
+          <div className="mb-5">
+            <span className="text-8xl font-bold flex items-end">20+</span>
+          </div>
+
+          <div className="flex flex-col text-lg tracking-widest font-semibold justify-start">
+            <span>Ans</span>
+            <span>d'expériences</span>
+            <span>professionnelles</span>
+          </div>
         </div>
 
-        <p className="col-span-9">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
-          corrupti recusandae? Dolorem, reiciendis. Recusandae, optio tempore
-          ipsa ad sed rem nisi dolore dolores, ducimus, possimus quis animi eius
-          repellat quasi.
-        </p>
+        <div className="col-span-9">
+          <p className="mb-10">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+            corrupti recusandae? Dolorem, reiciendis. Recusandae, optio tempore
+            ipsa ad sed rem nisi dolore dolores, ducimus, possimus quis animi
+            eius repellat quasi.
+          </p>
 
-        <div className="col-span-3 flex flex-col text-lg tracking-widest font-semibold justify-start">
-          <span>Années</span>
-          <span>d'expériences</span>
-          <span>professionnelles</span>
-        </div>
-
-        <div className="col-span-9 flex gap-x-5">
-          {specialities.map((speciality) => (
-            <div className="flex flex-col gap-y-2 bg-slate-900 p-5">
-              <div className="bg-amber-400 w-fit p-2 text-2xl text-slate-800 rounded-xl">
-                {speciality.icon}
+          <div className="flex gap-x-5">
+            {specialities.map((speciality) => (
+              <div className="flex flex-col gap-y-2 bg-slate-900 p-5 rounded-lg">
+                <div className="bg-amber-400 w-fit p-2 text-2xl text-slate-800 rounded-xl">
+                  {speciality.icon}
+                </div>
+                <h3 className="font-semibold">{speciality.title}</h3>
+                <p className="text-sm text-slate-100/80">
+                  {speciality.description}
+                </p>
               </div>
-              <h3 className="text-lg font-semibold">{speciality.title}</h3>
-              <p className="text-sm text-slate-100/80">
-                {speciality.description}
-              </p>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
 
       <div className="grid grid-cols-3 gap-10 py-10 px-10 bg-slate-900">
         {metrics.map((metric) => (
-          <div className="flex flex-col text-center p-5 bg-slate-800">
+          <div className="flex flex-col text-center p-5 bg-slate-800 rounded-lg">
             <span className="text-3xl font-bold">{metric.metric}</span>
             <span className="text-sm">{metric.description}</span>
           </div>
