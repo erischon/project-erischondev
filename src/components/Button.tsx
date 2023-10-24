445538ae0d132a9eeb58fea75aa86bf49ae316c2
erischon <66017574+erischon@users.noreply.github.com>
2023-10-24 16:21:46 +0200

    contacts section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,13 @@
-export default function Button({ children }: { children: React.ReactNode }) {
+type ButtonProps = {
+  children: React.ReactNode;
+  styles?: string;
+};
+
+export default function Button({ children, styles }: ButtonProps) {
   return (
-    <button className="bg-amber-400 text-slate-900 font-semibold py-1 px-2 md:py-2 md:px-4 rounded-md border-2 hover:border-2 border-amber-400 hover:bg-slate-900 hover:text-slate-300 transition-all">
+    <button
+      className={`bg-amber-400 text-slate-900 font-semibold py-1 px-2 md:py-2 md:px-4 rounded-md border-2 hover:border-2 border-amber-400 hover:bg-slate-900 hover:text-slate-300 transition-all ${styles}`}
+    >
       {children}
     </button>
   );
---
+++ src/data/contactsInfo.js
@@ -0,0 +1,14 @@
+import { HiOutlinePhone, HiOutlineMail } from "react-icons/hi";
+
+export const contactsInfo = [
+  {
+    name: "Téléphone",
+    icon: <HiOutlinePhone />,
+    info: "06 12 34 56 78",
+  },
+  {
+    name: "Email",
+    icon: <HiOutlineMail />,
+    info: "erischon@gmail.com",
+  },
+];
